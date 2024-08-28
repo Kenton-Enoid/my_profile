4c4e17731794ddc59694610a525e20fce921745b
Bonolo Gaosenkwe <bonolo.kamolane@gmail.com>
2024-08-28 10:08:49 +0200

    Railway Update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,9 +27,9 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1","kentonprofile.up.railway.app"]
-CSRF_TRUSTED_ORIGINS = ['https://kentonprofile.up.railway.app']
-
+# ALLOWED_HOSTS = ["localhost", "127.0.0.1","kentonprofile.up.railway.app"]
+# CSRF_TRUSTED_ORIGINS = ['https://kentonprofile.up.railway.app']
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
