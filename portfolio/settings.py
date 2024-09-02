8dfc3d007ba6396aecf488f988df3c8aa04fbcba
Bonolo Gaosenkwe <bonolo.kamolane@gmail.com>
2024-09-02 10:06:33 +0200

    Edited settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Add STATICFILES_DIRS to specify the directory where static files are located
 STATIC_URL = 'static/'
-STATICFILES_DIRS = BASE_DIR/'static'
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATICFILES_DIRS = [BASE_DIR/'static']
+STATIC_ROOT = [BASE_DIR/'staticfiles']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
