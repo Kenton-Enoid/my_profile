53d6fbab15b9131149054a01add76fbeaf9743bd
Bonolo Gaosenkwe <bonolo.kamolane@gmail.com>
2024-09-03 10:10:20 +0200

    Allowed all hosts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [".vercel.app"] 
+ALLOWED_HOSTS = ["*", ".vercel.app"] 
 
 # Application definition
 
